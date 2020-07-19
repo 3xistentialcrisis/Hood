0bd576d58cb3482352b30bcbeb41a2b9260a743c
Wanjiku Karanja <ciiku92@gmail.com>
2020-07-19 18:06:02 +0300

    Create Neighbourhood Model with location and name properties

--- hoodapp/models.py
+++ hoodapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+#Neighbourhood 
+class Neighbourhood(models.Model):
+    neighbourhood= models.CharField(max_length=100)
+    location=models.CharField(max_length=30)
+    
