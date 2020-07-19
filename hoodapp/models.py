aa46afb8a8de323230319ffcfc88c2c2eddb7477
Wanjiku Karanja <ciiku92@gmail.com>
2020-07-19 18:42:43 +0300

    Add Create & Delete  Neighbourhood functions

--- hoodapp/models.py
+++ hoodapp/models.py
@@ -7,4 +7,13 @@ class Neighbourhood(models.Model):
     neighbourhood_name= models.CharField(max_length=100)
     location=models.CharField(max_length=30)
     population=models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return self.Neighborhood
+
+    def create_neighbourhood(self):
+        self.save()
+
+    def delete_neighbourhood(self):
+        self.delete()
     
