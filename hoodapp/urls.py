2f7a698607632a1c994a3fb706f060ce5a5f79f1
Wanjiku Karanja <ciiku92@gmail.com>
2020-07-20 11:19:31 +0300

    Deploy to Heroku

--- hoodapp/urls.py
+++ hoodapp/urls.py
@@ -17,8 +17,8 @@ urlpatterns = [
     url(r'account/', include('django.contrib.auth.urls')),
     url(r'search/', views.search_business, name='search'),
     url(r'^post/', views.new_post, name='new_post'),
-    url(r'^all_businesses/',views.businesses, name='businesses'),
-    url(r'^new/business/$',views.new_business, name='new_business'),
+    url(r'^businesses/',views.businesses, name='businesses'),
+    url(r'^new_business/$',views.new_business, name='new_business'),
 
 ]
 
