c2b21ce627396fb5431a8c788f68959d049b3175
Wanjiku Karanja <ciiku92@gmail.com>
2020-07-20 10:32:19 +0300

    Create test to test neighbourhood

--- hoodapp/tests.py
+++ hoodapp/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from .models import Neighbourhood, Profile, Business
 
 # Create your tests here.
+class NeighbourhoodTestClass(TestCase):
+    def setUp(self):
+        self.neighbourhood = Neighbourhood(neighbourhood_name = 'Westlands')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.neighbourhood,Neighbourhood))
